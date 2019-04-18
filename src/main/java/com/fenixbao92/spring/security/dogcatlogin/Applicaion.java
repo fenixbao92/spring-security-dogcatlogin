f3f2ed18d48d636d4badfdb7ebe2555f10f14b74
fenixbao92 <fenixbao92@gmail.com>
2019-04-18 15:07:25 +0800

    project init.

---
+++ src/main/java/com/fenixbao92/spring/security/dogcatlogin/Applicaion.java
@@ -0,0 +1,13 @@
+package com.fenixbao92.spring.security.dogcatlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Applicaion {
+
+    public static void main(String[] args){
+        SpringApplication.run(Applicaion.class, args);
+        System.out.println("启动成功");
+    }
+
+}
